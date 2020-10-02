8c07ae256b61918fcf33673c5f0987b8d6114d60
Andy Collins <askingalot@gmail.com>
2020-10-02 08:01:45 -0500

    search posts

--- Gifter/Controllers/PostController.cs
+++ Gifter/Controllers/PostController.cs
@@ -50,6 +50,11 @@ namespace Gifter.Controllers
             return Ok(post);
         }
 
+        [HttpGet("search")]
+        public IActionResult Search(string q, bool sortDesc)
+        {
+            return Ok(_postRepository.Search(q, sortDesc));
+        }
 
         [HttpPost]
         public IActionResult Post(Post post)

542f0760793810f17ed41e5877453749f5a12c90
James Su-Shum <sushumjames@gmail.com>
2020-10-15 15:55:11 -0400

    Unit test passing UserProfileController Get HTTP method

---
+++ Gifter.Tests/Mocks/InMemoryUserProfileRepository.cs
@@ -0,0 +1,58 @@
+﻿using Gifter.Models;
+using Gifter.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gifter.Tests.Mocks
+{
+    class InMemoryUserProfileRepository : IUserProfileRepository
+    {
+        private readonly List<UserProfile> _data;
+
+        // Public property returning list of user profiles
+        public List<UserProfile> InternalData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        // Constructor will require a list of user profiles to be passed-in to fill _data
+        public InMemoryUserProfileRepository(List<UserProfile> startingData)
+        {
+            _data = startingData;
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _data;
+        }
+
+        public UserProfile GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProfile GetByIdWithPosts(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UserProfile userProfile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gifter.Tests/UserProfileControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Gifter.Controllers;
+using Gifter.Models;
+using Gifter.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Gifter.Tests
+{
+    public class UserProfileControllerTests
+    {
+        [Fact]
+        public void Get_Returns_All_UserProfiles()
+        {
+            // Arrange
+            var userProfileCount = 10;
+            var userProfiles = CreateTestUserProfiles(userProfileCount);
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUserProfiles = Assert.IsType<List<UserProfile>>(okResult.Value);
+
+            // Verifying result count equals the user profile count 
+            Assert.Equal(userProfileCount, actualUserProfiles.Count);
+            // Verifying result user profiles list equals user profiles list
+            Assert.Equal(userProfiles, actualUserProfiles);
+        }
+
+        // Method creating/returning list of dummy user profiles depending on count
+        private List<UserProfile> CreateTestUserProfiles(int count)
+        {
+            var userProfiles = new List<UserProfile>();
+            for (var i = 1; i <= count; i++)
+            {
+                userProfiles.Add(new UserProfile()
+                {
+                    Id = i,
+                    Name = $"User {i}",
+                    Email = $"user{i}@example.com",
+                    Bio = $"Bio {i}",
+                    DateCreated = DateTime.Today.AddDays(-i),
+                    ImageUrl = $"http://user.image.url/{i}"
+                });
+            }
+            return userProfiles;
+        }
+    }
+}
